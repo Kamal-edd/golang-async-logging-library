13af288146bc3846725a85834d2dccd6524d90ef
kamal eddine <kamal@alphorm.com>
2022-08-09 20:12:43 +0100

    Refactor error reporting

--- alog.go
+++ alog.go
@@ -57,7 +57,9 @@ func (al Alog) write(msg string, wg *sync.WaitGroup) {
 	defer al.m.Unlock()
 	_, err := al.dest.Write([]byte(al.formatMessage(msg)))
 	if err != nil {
-		al.errorCh <- err
+		go func(err error) {
+			al.errorCh <- err
+		}(err)
 	}
 }
 
