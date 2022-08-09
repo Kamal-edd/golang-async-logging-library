dbf2cb2b4e24e91b383a1cb8a7a9ff963cb3fd14
kamal eddine <kamal@alphorm.com>
2022-08-09 20:58:52 +0100

    Wait for all log entries before shutdown

--- alog.go
+++ alog.go
@@ -41,12 +41,15 @@ func New(w io.Writer) *Alog {
 // Start begins the message loop for the asynchronous logger. It should be initiated as a goroutine to prevent
 // the caller from being blocked.
 func (al Alog) Start() {
+	wg := &sync.WaitGroup{}
 loop:
 	for {
 		select {
 		case msg := <-al.msgCh:
-			go al.write(msg, nil)
+			wg.Add(1)
+			go al.write(msg, wg)
 		case <-al.shutdownCh:
+			wg.Wait()
 			al.shutdown()
 			break loop
 		}
@@ -69,6 +72,7 @@ func (al Alog) write(msg string, wg *sync.WaitGroup) {
 			al.errorCh <- err
 		}(err)
 	}
+	wg.Done()
 }
 
 func (al Alog) shutdown() {
